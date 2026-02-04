ced1e4f64cb806a88f4a19b38fd7774831f87ec0
researchmnglbnk-ui <researchmnglbnk@gmail.com>
2026-02-04 09:27:50 +0800

    Update data_automation.py

--- data_automation.py
+++ data_automation.py
@@ -260,7 +260,7 @@ def main():
         pv_population.to_excel(writer, sheet_name="Population", index=False)
 
         # ===================== LOAD TO BIGQUERY (RAW, NO CHANGE) =====================
-    table_id = "astute-azimuth-485909-p6.Automation_data.test_table"
+    table_id = "mongol-bank-macro-data.Automation_data.fact_macro"
 
     # Wide → Long (ямар ч drop / filter хийхгүй)
     id_col = "ОН"
